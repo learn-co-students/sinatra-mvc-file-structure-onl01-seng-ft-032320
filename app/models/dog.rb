c5a6105d926c1aacc73dc42c7e3fe5bd520b9b8c
javante_nelson <javantenelson@gmail.com>
2020-05-01 23:29:20 -0500

    Done.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,14 +4,15 @@ class Dog
     
     @@all = []
 
-    def initialize(name,breed,age)
+    def initialize(name, breed, age)
         @name = name 
         @breed = breed 
-        @age = age
-        self.all << self  
+        @age = age 
+        self.class.all << self  
     end 
 
     def self.all
+        binding.pry 
         @@all 
     end
 end 
